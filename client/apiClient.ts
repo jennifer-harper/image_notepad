8a560dcfd5caa4598af114dbda7dc6f1f6b8f2f0
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-05-04 09:10:18 +1200

    commit before branch

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -62,13 +62,17 @@ export function getUnsplash() {
 
 export async function defineUnsplash(query: string){
   try {
-    const res1 = await request.get(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_KEY}&per_page=12&orientation=landscape&query=${query}`);
-    const totalPages = res1.body.total_pages;
-    const randomPageNumber = Math.floor(Math.random() * totalPages) + 1;
-    const res2 = await request.get(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_KEY}&page=${randomPageNumber}&per_page=12&orientation=landscape&query=${query}`);
-    console.log(res2.body);
-    return res2.body;
+    const res1 = await request
+    .get(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_KEY}&per_page=12&orientation=landscape&query=${query}`)
+
+    const totalPages = res1.body.total_pages
+    const randomPageNumber = Math.floor(Math.random() * totalPages) + 1
+
+    const res2 = await request.get(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_KEY}&page=${randomPageNumber}&per_page=12&orientation=landscape&query=${query}`)
+    console.log(res2.body)
+    return res2.body
+
   } catch (err) {
-    console.log('Err message: ' + err);
+    console.log('Err message: ' + err)
   }
 }
