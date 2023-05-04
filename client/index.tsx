974322ff6a1cedf1240eaa0669e2ebfcea317187
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-05-04 18:34:23 +1200

    added redux

--- client/index.tsx
+++ client/index.tsx
@@ -1,11 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import App from './components/App'
+import store from './store'
+
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <Router>
-      <App />
-  </Router>
+    <Provider store={store}>
+      <Router>
+          <App />
+      </Router>
+    </Provider>
   )
 })
---
+++ client/reducers/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+
+import searchImg from './searchImg'
+
+export default combineReducers({
+    searchImg
+})
