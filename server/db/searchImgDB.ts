994876887f545bb59d833d0010f4b3dd6227bc33
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-05-04 14:40:34 +1200

    create and delete added

--- server/db/searchImgDB.ts
+++ server/db/searchImgDB.ts
@@ -5,3 +5,12 @@ import * as Img from '../../models/character'
 export function getAllImgsDB(db = connection): Promise<Img.ImgSearch[]>{
     return db('save-search').select()
 }
+
+export function delImgDB (id:number, db = connection): Promise<number>{
+    return db('save-search').delete().where('id', id)
+}
+
+export function createImgDB (data:Img.ImgSearchData, db = connection): Promise<Img.ImgSearchData[]>{
+    return db('save-search').insert(data)
+    .returning('*')
+}
--- server/routes/savedImgs.ts
+++ server/routes/savedImgs.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as db from '../db/searchImgDB'
+// import * as Img from '../../models/character'
 const router = express.Router()
 
 
@@ -12,6 +13,31 @@ db.getAllImgsDB()
     res.status(500).send(err.message)
     })
 })
+
+
+  //*******************Delete existing
+  router.delete('/:id', (req,res) =>{
+    db.delImgDB (+req.params.id)
+    .then(() => {
+      res.sendStatus(200) 
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })  
+  })
+
+  //*******************Create existing
+
+
+  router.post('/', (req, res) => {
+   db.createImgDB(req.body)
+      .then((data) => {
+        res.json(data)
+      })
+      .catch((err) => {
+        res.status(500).send(err.message)
+      })
+  })
 
 
 export default router
