bfe4738dfc62d1c243ea8df24dbcc1ec4f6d2911
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-06-01 11:32:06 +1200

    try to fix secret for railway

--- server/db/connections.ts
+++ server/db/connections.ts
@@ -7,5 +7,12 @@ const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 const connection = knex(config)
 
+
+
+if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    const envConfig = require('dotenv').config()    
+    if(envConfig.error) throw envConfig.error    
+}
+    
     
 export default connection
