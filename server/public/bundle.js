105bf14a7f9ceebfb09fd93cd9086461568eca83
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-06-13 09:59:10 +1200

    add key back in to env

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -4312,7 +4312,7 @@ __webpack_require__.r(__webpack_exports__);
 
 //******************Unsplash API/
 function getUnsplash() {
-  return superagent__WEBPACK_IMPORTED_MODULE_0___default().get(`https://api.unsplash.com/photos/random/?client_id=${process.env.UNSPLASH_KEY}&orientation=landscape`).then(res => {
+  return superagent__WEBPACK_IMPORTED_MODULE_0___default().get(`https://api.unsplash.com/photos/random/?client_id=${"48xo5zkXZ6Sh8EYO_etxPja_x05J4Zyhitdp7r7VH38"}&orientation=landscape`).then(res => {
     console.log(res.body);
     return res.body;
   }).catch(err => {
@@ -4321,10 +4321,10 @@ function getUnsplash() {
 }
 async function SearchSaveUnsplash(query) {
   try {
-    const res1 = await superagent__WEBPACK_IMPORTED_MODULE_0___default().get(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_KEY}&per_page=12&orientation=landscape&query=${query}`);
+    const res1 = await superagent__WEBPACK_IMPORTED_MODULE_0___default().get(`https://api.unsplash.com/search/photos?client_id=${"48xo5zkXZ6Sh8EYO_etxPja_x05J4Zyhitdp7r7VH38"}&per_page=12&orientation=landscape&query=${query}`);
     const totalPages = res1.body.total_pages;
     const randomPageNumber = Math.floor(Math.random() * totalPages) + 1;
-    const res2 = await superagent__WEBPACK_IMPORTED_MODULE_0___default().get(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_KEY}&page=${randomPageNumber}&per_page=12&orientation=landscape&query=${query}`);
+    const res2 = await superagent__WEBPACK_IMPORTED_MODULE_0___default().get(`https://api.unsplash.com/search/photos?client_id=${"48xo5zkXZ6Sh8EYO_etxPja_x05J4Zyhitdp7r7VH38"}&page=${randomPageNumber}&per_page=12&orientation=landscape&query=${query}`);
     console.log(res2.body);
     return res2.body;
   } catch (err) {
